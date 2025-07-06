9670b2fcd4e173a92a25b5988d8ce00b336bd020
sivapriyaa <sivapriyaa2006@gmail.com>
2025-07-06 17:59:02 +0530

    Removed secret from history

--- frontend/app.py
+++ frontend/app.py
@@ -21,7 +21,7 @@ if user_input:
 
     with st.chat_message("assistant"):
         print("Sending to backend:", {"message": user_input})
-        res = requests.post("https://calendar-xut2.onrender.com", json={"message": user_input})
+        res = requests.post("https://calendar-1-ouhh.onrender.com", json={"message": user_input})
         print("📤 Sent to backend:", {"message": user_input})
         bot_reply = res.json().get("response", "Something went wrong.")
         st.markdown(bot_reply)
